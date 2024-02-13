d6d6850cdc0add28e915d0259200cb8761d8c431
manoj-gamage <158369162+manoj-gamage@users.noreply.github.com>
2024-02-13 15:42:27 -0600

    Hot fix (#1)
    * hot fix from user 1
    
    * add user from user 1
    
    * add product from user 1
    
    * Add name by user 1
    
    * add ssn by user 1
    
    * Add address by user 1
    
    ---------
    
    Co-authored-by: Manoj Gamage <manoj.gamage@qtcm.com>

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace git_training.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Manoj Gamage";
+            ViewBag.Id = "SSN";
+            ViewBag.Address = "9939";
+            return View();
+        }
+    }
+}
---
+++ Controllers/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace git_training.Controllers
+{
+    public class User : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
