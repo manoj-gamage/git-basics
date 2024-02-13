2faf7fdb076b219a58c8ef917338831c537f7d9a
Manoj Gamage <manoj.gamage@qtcm.com>
2024-02-13 15:52:29 -0600

    add set 3.2

--- Controllers/User.cs
+++ Controllers/User.cs
@@ -7,6 +7,7 @@ namespace git_training.Controllers
         public IActionResult Index()
         {
             ViewBag.Name = "Manoj Gamage";
+            ViewBag.Name = "Manoj Gamage";
             return View();
         }
     }
