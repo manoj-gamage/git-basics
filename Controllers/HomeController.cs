30792601565955c1a5e1a0a85268dd58186fce61
Manoj Gamage <manoj.gamage@qtcm.com>
2024-02-13 15:54:19 -0600

    add set 3.1 master

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,6 +25,7 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         ViewBag.Player = "Manoj Gamage";
+        ViewBag.Player = "Manoj Gamage";
         return View();
     }
 
